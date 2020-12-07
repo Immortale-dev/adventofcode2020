af3789833564e4c5fa5ce2a893a1462fc5518649
Immortal <web.developer.ukraine@gmail.com>
2020-12-07 16:52:05 +0000

    day 7, improve day 4, add helpers

--- d4/a.cpp
+++ d4/a.cpp
@@ -1,26 +1,11 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
+#include "../helpers/string_helpers.cpp"
 
 typedef long long int ll;
 
 vector<unordered_map<string,string>> input;
-
-vector<string> split_line(string line, string delim)
-{
-	vector<string> res;
-	int pos = 0;
-	while(true){
-		int newpos = line.find(delim, pos);
-		if(newpos < 0){
-			res.push_back(line.substr(pos));
-			break;
-		}
-		res.push_back(line.substr(pos,newpos-pos));
-		pos = newpos+delim.size();
-	}
-	return res;
-}
 
 void prepare()
 {
